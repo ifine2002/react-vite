e29dedb963fab1464f5fd1537b3fef857e0ca6ee
ifine2002 <boyyhp1@gmail.com>
2024-11-30 12:25:32 +0700

    how to use variable

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,10 +4,19 @@
 //JSX
 //fragment
 import './style.css';
+
 const MyComponent = () => {
+    // const hoidanit = "eric1"; //string
+    // const hoidanit = 25; //number
+    const hoidanit = [1, 2, 3, 4];
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // }
     return (
         <>
-            <div> eric & hoidanit </div>
+            <div> {JSON.stringify(hoidanit)} & hoidanit </div>
+            <div>{console.log("Eric")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}>child</div>
         </>
