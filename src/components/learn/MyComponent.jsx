8454e214a1248b5f8bb67b52259b610b3ce4b138
ifine2002 <boyyhp1@gmail.com>
2024-11-30 12:12:12 +0700

    JSX, use css

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,16 @@
 //() => {}
 //component = html + css + js
+
+//JSX
+//fragment
+import './style.css';
 const MyComponent = () => {
     return (
-        <div> eric & hoidanit </div>
+        <>
+            <div> eric & hoidanit </div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}>child</div>
+        </>
     );
 }
 
