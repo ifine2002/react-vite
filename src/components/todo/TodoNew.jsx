3624d034713397e17dcbcd39ea007ded2e0008c1
ifine2002 <boyyhp1@gmail.com>
2024-11-30 19:05:27 +0700

    DOM event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,11 +2,23 @@
 const TodoNew = (props) => {
     console.log(">>>check props: ", props)
     const { addNewTodo } = props;
-    addNewTodo("eric");
+    // addNewTodo("eric");
+    const handleClick = () => {
+        alert("click me")
+    }
+    const handleChange = (name) => {
+        console.log(">>>> handleChange", name)
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
