c3c47ccd5e7e66f01e957388f7c186ba4e39ac3d
ifine2002 <boyyhp1@gmail.com>
2024-12-02 00:26:43 +0700

    design modal update user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,4 +1,6 @@
 import { Table } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import UpdateUserModal from './update.user.modal';
 
 const UserTable = (props) => {
 
@@ -8,6 +10,11 @@ const UserTable = (props) => {
         {
             title: 'Id',
             dataIndex: '_id',
+            render: (_, record) => {
+                return (
+                    <a href='#'>{record._id}</a>
+                )
+            }
         },
         {
             title: 'Full Name',
@@ -16,17 +23,30 @@ const UserTable = (props) => {
         {
             title: 'Email',
             dataIndex: 'email',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <div style={{ display: "flex", gap: "20px" }}>
+                    <EditOutlined style={{ cursor: "pointer", color: "orange" }} />
+                    <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                </div>
+            )
         }
     ];
 
 
 
     return (
-        <Table
-            columns={columns}
-            dataSource={dataUsers}
-            rowKey="_id"
-        />
+        <>
+            <Table
+                columns={columns}
+                dataSource={dataUsers}
+                rowKey="_id"
+            />
+            <UpdateUserModal />
+        </>
     )
 }
 
