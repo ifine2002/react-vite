9d8c4ce285e2fd56062b2c0d806a2d3173bab3fe
ifine2002 <boyyhp1@gmail.com>
2024-12-02 01:04:24 +0700

    useEffect with dependency

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,11 +1,14 @@
 import { Table } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import UpdateUserModal from './update.user.modal';
+import { useState } from 'react';
 
 const UserTable = (props) => {
 
     const { dataUsers } = props;
 
+    const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+    const [dataUpdate, setDataUpdate] = useState(null);
     const columns = [
         {
             title: 'Id',
@@ -29,7 +32,12 @@ const UserTable = (props) => {
             key: 'action',
             render: (_, record) => (
                 <div style={{ display: "flex", gap: "20px" }}>
-                    <EditOutlined style={{ cursor: "pointer", color: "orange" }} />
+                    <EditOutlined
+                        onClick={() => {
+                            setDataUpdate(record);
+                            setIsModalUpdateOpen(true);
+                        }}
+                        style={{ cursor: "pointer", color: "orange" }} />
                     <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
                 </div>
             )
@@ -45,7 +53,12 @@ const UserTable = (props) => {
                 dataSource={dataUsers}
                 rowKey="_id"
             />
-            <UpdateUserModal />
+            <UpdateUserModal
+                isModalUpdateOpen={isModalUpdateOpen}
+                setIsModalUpdateOpen={setIsModalUpdateOpen}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+            />
         </>
     )
 }
