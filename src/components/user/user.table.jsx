89c0c5631aa2f2d6ddd34b9a21fa1129769553c9
ifine2002 <boyyhp1@gmail.com>
2024-12-02 14:52:10 +0700

    completed crud user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,8 +1,9 @@
-import { Table } from 'antd';
+import { notification, Popconfirm, Table } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import UpdateUserModal from './update.user.modal';
 import { useState } from 'react';
 import ViewUserDetail from './view.user.detail';
+import { deleteUserAPI } from '../../services/api.service';
 
 const UserTable = (props) => {
 
@@ -12,6 +13,7 @@ const UserTable = (props) => {
     const [dataUpdate, setDataUpdate] = useState(null);
     const [dataDetail, setDataDetail] = useState(null);
     const [isDetailOpen, setIsDetailOpen] = useState(false);
+
     const columns = [
         {
             title: 'Id',
@@ -47,13 +49,38 @@ const UserTable = (props) => {
                             setIsModalUpdateOpen(true);
                         }}
                         style={{ cursor: "pointer", color: "orange" }} />
-                    <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                    <Popconfirm
+                        title="Xóa người dùng"
+                        description="Bạn chắc chắn xóa user này?"
+                        onConfirm={() => handleDeleteUser(record._id)}
+                        okText="Yes"
+                        cancelText="No"
+                        placement='left'
+                    >
+                        <DeleteOutlined
+                            style={{ cursor: "pointer", color: "red" }} />
+                    </Popconfirm>
+
                 </div>
             )
         }
     ];
 
-
+    const handleDeleteUser = async (id) => {
+        const res = await deleteUserAPI(id);
+        if (res.data) {
+            notification.success({
+                message: "Delete User",
+                description: "Xóa user thành công"
+            })
+            await loadUser();
+        } else {
+            notification.error({
+                message: "Error Delete User",
+                description: JSON.stringify(res.message)
+            })
+        }
+    }
 
     return (
         <>
