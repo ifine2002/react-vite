1c791254a54bca7abaafc37d169ca63ebfd03420
ifine2002 <boyyhp1@gmail.com>
2024-12-03 01:15:05 +0700

    user uncontrolled component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,76 @@
+import { Button, Input, Form } from "antd";
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log(">>>> check value: ", values)
+    }
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+            <div style={{
+                margin: "50px",
+            }}>
+                <Form.Item
+                    label="Full Name "
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your password!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your password!',
+                //     },
+                // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your password!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button
+                        onClick={() => form.submit()}
+                        type="primary">Register</Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 
